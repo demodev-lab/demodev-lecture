75956bce1a7fa329a2dadac3952d7321608daa3f
minzoo <aw12813@gmail.com>
2025-07-10 11:00:11 +0900

    feat: 챌린지 데이터 수정 및 링크 연결

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import CategoryNavigation from "@/components/CategoriNavigation";
 import CourseSection from "@/components/CourseSection";
 import Footer from "@/components/Footer";
 import {
-  newCourses,
+  challenges,
   popularCourses,
   editorChoiceCourses,
   hobbyCourses,
@@ -20,11 +20,17 @@ export default function Home() {
 
       <main className="container mx-auto px-4 py-8">
         <div className="space-y-12">
-          <CourseSection title="신규 오픈 강좌" courses={newCourses} />
+          <CourseSection title="신규 오픈 챌린지" courses={challenges} />
           <CourseSection title="이번 주 인기 강좌" courses={popularCourses} />
-          <CourseSection title="에디터 추천 강좌" courses={editorChoiceCourses} />
+          <CourseSection
+            title="에디터 추천 강좌"
+            courses={editorChoiceCourses}
+          />
           <CourseSection title="원포인트 취미 강좌" courses={hobbyCourses} />
-          <CourseSection title="지금 뜨는 자격증 강좌" courses={certificationCourses} />
+          <CourseSection
+            title="지금 뜨는 자격증 강좌"
+            courses={certificationCourses}
+          />
         </div>
       </main>
 
